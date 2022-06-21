f285d7837d8957f58f42ab7daa29606438f5b24d
raxmatjon <82531443+romanurmatov1@users.noreply.github.com>
2022-06-21 21:04:53 +0500

    Finish #1 | Send Email Project

---
+++ #1_Send_Email/SendEmail/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.core.mail import send_mail
+
+def index(request):
+    if request.method == 'POST':
+        name = request.POST.get('full-name')
+        email = request.POST.get('email')
+        subject = request.POST.get('subject')
+        message = request.POST.get('message')
+
+        data = {
+            'name' : name,
+            'email': email,
+            'subject': subject,
+            'message': message
+        }
+        message = '''
+        New message: {}
+        
+        From: {}
+        '''.format(data['message'], data['email'])
+        send_mail(data['subject'], message, '', [email])
+    return render(request, 'SendEmail/index.html', {})
